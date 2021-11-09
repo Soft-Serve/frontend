78d721d9513ffadce66ffa89df95c683197eb1a1
Kristine McBride <kristinemcbride@Kristines-MacBook-Pro.local>
2021-11-09 22:24:45 +0100

    fix frontend

--- src/client/index.ts
+++ src/client/index.ts
@@ -1,41 +1,36 @@
-import { ApolloClient, ApolloLink, InMemoryCache } from "@apollo/client";
+import { ApolloClient, InMemoryCache } from "@apollo/client";
 import { RestLink } from "apollo-link-rest";
+import { setContext } from "@apollo/client/link/context";
 import { UID, ACCESS_TOKEN, CLIENT_TOKEN } from "src/constants";
 
-const authRestLink = new ApolloLink((operation, forward) => {
-  operation.setContext(({ headers }: any) => {
-    const token = localStorage.getItem(ACCESS_TOKEN);
-    return {
-      headers: {
-        ...headers,
-        Accept: "application/json",
-        "Access-Control-Allow-Origin": "*",
-        "Access-Control-Allow-Headers": "origin, content-type ",
-        credentials: "same-origin",
-        mode: "no-cors",
-        Authorization: token,
-        client: localStorage.getItem(CLIENT_TOKEN) || "",
-        uid: localStorage.getItem(UID) || "",
-      },
-    };
-  });
-  return forward(operation).map(result => {
-    const { restResponses } = operation.getContext();
-    const authTokenResponse = restResponses.find(
-      (res: { headers: { has: (arg0: string) => any } }) => res.headers.has("Authorization")
-    );
-    if (authTokenResponse) {
-      localStorage.setItem(CLIENT_TOKEN, authTokenResponse.headers.get("Authorization"));
-    }
-    return result;
-  });
+const uri =
+  process.env.NODE_ENV === "production"
+    ? process.env.REACT_APP_PROD_API_URL
+    : process.env.REACT_APP_DEV_API_URL;
+
+const link = new RestLink({
+  uri,
+  headers: {
+    Accept: "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.",
+    "Content-Type": "application/json; charset=utf-8",
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Headers": "*",
+  },
 });
 
-const restLink = new RestLink({ uri: "https://please-work-soft-serve.herokuapp.com/" });
+const authLink = setContext((_, { headers }) => {
+  return {
+    headers: {
+      ...headers,
+      "access-token": localStorage.getItem(ACCESS_TOKEN) || "",
+      client: localStorage.getItem(CLIENT_TOKEN) || "",
+      uid: localStorage.getItem(UID) || "",
+    },
+  };
+});
 
 const client = new ApolloClient({
+  link: authLink.concat(link),
   cache: new InMemoryCache(),
-  link: ApolloLink.from([authRestLink, restLink]),
 });
-
 export { client };
