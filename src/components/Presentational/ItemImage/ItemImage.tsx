d8deae1d30abd1d36a74a26e325b4fee05966419
Kristine McBride <kristinemcbride@Kristines-MacBook-Pro.local>
2021-11-17 22:36:17 -0500

    fix soldout logic

--- src/components/Presentational/ItemImage/ItemImage.tsx
+++ src/components/Presentational/ItemImage/ItemImage.tsx
@@ -4,6 +4,7 @@ import { AdvancedImage } from "@cloudinary/react";
 import { Cloudinary, CloudinaryImage } from "@cloudinary/base";
 import { fill } from "@cloudinary/base/actions/resize";
 import { Plugins } from "@cloudinary/html";
+import { Effect, sepia } from "@cloudinary/base/actions/effect";
 
 export interface ImgProps {
   cldImg: CloudinaryImage;
@@ -13,9 +14,10 @@ export interface ImgProps {
 
 interface Props extends Omit<ImgProps, "cldImg"> {
   photoUrl: string;
+  unavailable?: boolean;
 }
 
-const ItemImage: FC<Props> = ({ photoUrl, ...rest }) => {
+const ItemImage: FC<Props> = ({ photoUrl, unavailable, ...rest }) => {
   const cld = new Cloudinary({
     cloud: {
       cloudName: "softserve",
@@ -24,6 +26,10 @@ const ItemImage: FC<Props> = ({ photoUrl, ...rest }) => {
 
   const cldImage = cld.image(photoUrl);
   cldImage.resize(fill().width(192).height(192));
+
+  if (unavailable) {
+    cldImage.effect(Effect.grayscale());
+  }
 
   if (photoUrl) {
     return <AdvancedImage {...rest} cldImg={cldImage} />;
