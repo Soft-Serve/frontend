04720a97153b8b26a50ce965e0308d7d3ec7e88a
Kristine McBride <kristinemcbride@Kristines-MacBook-Pro.local>
2022-01-02 12:35:03 -0500

    landing page

--- src/views/LandingPage/LandingPage.tsx
+++ src/views/LandingPage/LandingPage.tsx
@@ -355,7 +355,7 @@ const LandingPage: FC = () => {
               </div>
               <div className="relative px-4 py-16 sm:px-6 sm:py-24 lg:py-32 lg:px-8">
                 <h1 className="text-center text-4xl font-extrabold tracking-tight sm:text-5xl lg:text-6xl">
-                  <span className="block text-white font-Quicksand">stylish software</span>
+                  <span className="block text-white font-Quicksand">modern software</span>
                   <span className="block text-red-200 font-Quicksand">
                     for the service industry
                   </span>
@@ -604,18 +604,6 @@ const LandingPage: FC = () => {
       </main>
       <footer className="bg-red-400">
         <div className="max-w-7xl mx-auto py-12 px-4 overflow-hidden sm:px-6 lg:px-8">
-          <nav className="-mx-5 -my-2 flex flex-wrap justify-center" aria-label="Footer">
-            {navigation.main.map(item => (
-              <div key={item.name}>
-                <a
-                  href={item.href}
-                  className="text-base text-white hover:text-red-400 hover:bg-white px-5 py-2 rounded-md font-Quicksand font-bold"
-                >
-                  {item.name}
-                </a>
-              </div>
-            ))}
-          </nav>
           <div className="mt-8 flex justify-center space-x-6">
             {navigation.social.map(item => (
               <a key={item.name} href={item.href} className="text-gray-400 hover:text-gray-500">
