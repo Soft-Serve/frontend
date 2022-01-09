ba91ab605ce17c3df7f991942b2c37c17fe28381
shahynkamali <shahynkamali@gmail.com>
2022-01-09 12:19:14 -0500

    main

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,13 @@ import { ApolloProvider } from "@apollo/client";
 
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { LoadingScreen } from "@base";
+import { Main, PageNotFound } from "@views";
 import { routes } from "@routes";
 import { client } from "./client";
-import { PageNotFound } from "./views/PageNotFound";
 
 const LandingPage = lazy(() => import("./views/LandingPage"));
 const SignUpPage = lazy(() => import("./views/SignUpPage/DefaultSignUpPage"));
 const ConfirmEmailPage = lazy(() => import("./views/ConfirmEmailPage/DefaultConfirmEmailPage"));
-const MainPage = lazy(() => import("./views/MainDefault"));
 
 const App = () => {
   return (
@@ -22,7 +21,7 @@ const App = () => {
             <Route path="/" element={<LandingPage />} />
             <Route path="/sign-up" element={<SignUpPage />} />
             <Route path="/confirm" element={<ConfirmEmailPage />} />
-            <Route path={`${routes.restaurants}/:id/*`} element={<MainPage />} />
+            <Route path={`${routes.restaurants}/:id/*`} element={<Main />} />
           </Routes>
         </Suspense>
       </Router>
--- src/views/index.ts
+++ src/views/index.ts
@@ -1,1 +1,2 @@
 export { Main } from "./Main";
+export { PageNotFound } from "./PageNotFound";
