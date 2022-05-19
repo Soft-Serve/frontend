ac4691a7cc597f4b167d56241b87eeee35cae2d9
Kristine McBride <kristinelmcbride@gmail.com>
2022-05-19 19:19:57 -0400

    add svgs

--- src/components/Base/DietarySvg/svgs/index.ts
+++ src/components/Base/DietarySvg/svgs/index.ts
@@ -5,3 +5,8 @@ export { ReactComponent as VeganSVG } from "./vegan.svg";
 export { ReactComponent as VegetarianSVG } from "./vegetarian.svg";
 export { ReactComponent as ShellfishSVG } from "./shellfish.svg";
 export { ReactComponent as NutSVG } from "./nuts.svg";
+export { ReactComponent as SesameSVG } from "./sesame.svg";
+export { ReactComponent as EggsSVG } from "./eggs.svg";
+export { ReactComponent as PeanutsSVG } from "./peanuts.svg";
+export { ReactComponent as SeafoodSVG } from "./seafood.svg";
+export { ReactComponent as HalalSVG } from "./halal.svg";
